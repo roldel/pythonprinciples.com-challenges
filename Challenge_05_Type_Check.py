f446d62700f68d28d43de955abea015e740644cb
roldel <74282569+RolandDel@users.noreply.github.com>
2021-04-02 03:32:45 +0000

    Add challenges 05 to 07

---
+++ Challenge_05_Type_Check.py
@@ -0,0 +1,11 @@
+'''Type check
+
+Write a function named only_ints that takes two parameters. 
+Your function should return True if both parameters are integers,
+and False otherwise.
+
+For example, calling only_ints(1, 2) should return True,
+while calling only_ints("a", 1) should return False.'''
+
+def only_ints(x,y):
+    return type(x) is int and type(y) is int
